b56c42a0167010e9bf4a1bdd2302b368a1dff023
RoyaldomSalim20 <khaerulsalim20@gmail.com>
2026-09-17 16:22:45 +0800

    jenis tipe data

---
+++ pertemuan2.py
@@ -0,0 +1,49 @@
+a = 5
+b = 10
+c = a + b
+print(c)
+
+a = 1.53
+b = 7.2
+c = a + b
+print(a + b)
+
+nama = "Larry"
+hobi = "ngoding"
+print(nama)
+print(hobi)
+
+teks = "Rahmat Flowchart"
+
+#menampilkan index ke-3
+print(teks[3])
+#menampilkan index ke-3 hingga ke-6
+print(teks[3:6])
+#menampilkan seluruhnya, tetapi dimulai dari index ke-7
+print(teks[7:])
+#menampilkan hanya sampai index ke-6
+print(teks[:6])
+
+mata_kuliah = ["APD","Logika Matematika","Kalkulus"]
+print(mata_kuliah[0])
+print(mata_kuliah[1])
+print(mata_kuliah[2])
+
+buku = {
+'judul' : 'Atomic Habits',
+'penulis' : 'James Clear',
+'halaman' : 320
+}
+
+# Mengakses Value dalam Dictionary
+print(buku['judul'])
+print(buku['penulis'])
+print(buku['halaman'])
+
+nama = input("Masukan nama anda: ")
+print(nama)
+
+a = int(input("masukkan nilai anda a:"))
+b = int(input("masukkan nilai anda b:"))
+c = int(a) + int(b)
+print(c)
